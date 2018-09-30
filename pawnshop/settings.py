e777e508e3f0f01df84a0767b60a3f3e7a1a54f0
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2018-09-30 22:48:39 +0800

    Setup logging, media file serving, views skeleton, and static & templates directory

--- pawnshop/settings.py
+++ pawnshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'transaction.apps.TransactionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'pawnshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,6 +69,29 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pawnshop.wsgi.application'
+
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django':  {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+        'transaction': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
 
 
 # Database
@@ -118,3 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static/media")
+MEDIA_URL = '/media/'
--- transaction/views.py
+++ transaction/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from . import models
+from .forms import *
+import logging
 
-# Create your views here.
+
+# enable logging in this application
+logger = logging.getLogger(__name__)
+
+
+# landing page for transaction application
+# require user to login before handling 'index' request
+@login_required(login_url='admin:login')
+def index(request):
+    return render(request, 'transaction/index.html')
+
+
+# handle transaction list request
+@login_required(login_url='admin:login')
+def transaction_list(request):
+    pass
+
+
+# handle transaction detail request
+@login_required(login_url='admin:login')
+def transaction_detail(request):
+    pass
+
+
+# handle transaction add request
+@login_required(login_url='admin:login')
+def transaction_add(request):
+    pass
+
+
+# handle transaction edit request
+@login_required(login_url='admin:login')
+def transaction_edit(request, pk):
+    pass
+
+
+# handle transaction delete request\
+@login_required(login_url='admin:login')
+def transaction_delete(request, pk):
+    pass
